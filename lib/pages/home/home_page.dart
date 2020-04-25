82b827cfe737c4a1e8310dd6adce63e3d6aaded5
henrique-nofv <henrique.nofv@gmail.com>
2020-04-25 16:45:52 -0300

    Added gesture detector to show/hide menu on move to up/down PageView Card

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:nubankclone/pages/home/widgets/card_app.dart';
 import 'package:nubankclone/pages/home/widgets/my_app_bar.dart';
 import 'package:nubankclone/pages/home/widgets/my_dots_app.dart';
 import 'package:nubankclone/pages/home/widgets/page_view_app.dart';
@@ -12,6 +11,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   bool _showMenu;
   int _currentIndex;
+  double _yPosition;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -23,6 +24,10 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     double _screenHeight = MediaQuery.of(context).size.height;
+    if (_yPosition == null) {
+      _yPosition = _screenHeight * .24;
+      print(_yPosition);
+    }
 
     return Scaffold(
       backgroundColor: Colors.purple[800],
@@ -34,20 +39,58 @@ class _HomePageState extends State<HomePage> {
               tap: () {
                 setState(() {
                   _showMenu = !_showMenu;
+                  _yPosition =
+                      _showMenu ? _screenHeight * .75 : _screenHeight * .24;
                 });
               }),
           PageViewApp(
-              top: _screenHeight * .24,
-              onChanged: (index) {
-                setState(() {
-                  _currentIndex = index;
-                });
-              }),
-          Positioned(
+            top: _yPosition,
+            showMenu: _showMenu,
+            onChanged: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            onPanUpdate: (details) {
+              double positionBottomLimit = _screenHeight * .75;
+              double positionTopLimit = _screenHeight * .24;
+              double midlePosition = positionBottomLimit - positionTopLimit;
+              midlePosition = midlePosition / 2;
+              setState(() {
+                _yPosition += details.delta.dy;
+
+                _yPosition = _yPosition < positionTopLimit
+                    ? positionTopLimit
+                    : _yPosition;
+
+                _yPosition = _yPosition > positionBottomLimit
+                    ? positionBottomLimit
+                    : _yPosition;
+
+                if (_yPosition != positionBottomLimit && details.delta.dy > 0) {
+                  _yPosition =
+                      _yPosition > positionTopLimit + midlePosition - 50
+                          ? positionBottomLimit
+                          : _yPosition;
+                }
+
+                if (_yPosition != positionTopLimit && details.delta.dy < 0) {
+                  _yPosition = _yPosition < positionBottomLimit - midlePosition
+                      ? positionTopLimit
+                      : _yPosition;
+                }
+
+                if (_yPosition == positionBottomLimit) {
+                  _showMenu = true;
+                } else if (_yPosition == positionTopLimit) {
+                  _showMenu = false;
+                }
+              });
+            },
+          ),
+          MyDotsApp(
             top: _screenHeight * .70,
-            child: MyDotsApp(
-              currentIndex: _currentIndex,
-            ),
+            currentIndex: _currentIndex,
           )
         ],
       ),
--- lib/pages/home/widgets/page_view_app.dart
+++ lib/pages/home/widgets/page_view_app.dart
@@ -5,17 +5,27 @@ import 'card_app.dart';
 class PageViewApp extends StatelessWidget {
   final double top;
   final ValueChanged<int> onChanged;
-  const PageViewApp({Key key, this.top, this.onChanged}) : super(key: key);
+  final GestureDragUpdateCallback onPanUpdate;
+  final bool showMenu;
+
+  const PageViewApp(
+      {Key key, this.top, this.onChanged, this.onPanUpdate, this.showMenu})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Positioned(
+    return AnimatedPositioned(
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeOut,
       top: top,
       height: MediaQuery.of(context).size.height * .45,
       left: 0,
       right: 0,
-      child: SizedBox(
+      child: GestureDetector(
+        onPanUpdate: onPanUpdate,
         child: PageView(
-          physics: BouncingScrollPhysics(),
+          physics: showMenu
+              ? NeverScrollableScrollPhysics()
+              : BouncingScrollPhysics(),
           onPageChanged: onChanged,
           children: <Widget>[
             CardApp(),
